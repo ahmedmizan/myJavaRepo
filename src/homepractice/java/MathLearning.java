7937a27c7017caa5b776975444671d67c499bb40
ahmedmizan <ahmedmmizan@gmail.com>
2020-06-20 13:54:55 -0400

    math learning updated

--- src/homepractice/java/MathLearning.java
+++ src/homepractice/java/MathLearning.java
@@ -2,17 +2,19 @@ package homepractice.java;
 
 public class MathLearning {
     public static void main(String[] args) {
-        int intValueOne = 50;
-        int intValueTwo = -20;
+//        int intValueOne = 50;
+//        int intValueTwo = -20;
+        int intValueThree = 50;
+        int intValueFour = 10;
 
 //        Double Variables
         double doubleValueOne =75.8;
         double doubleValueTwo =0.88 ;
 
 //        Example 1: Math.abs
-        System.out.println("Absolute value of integerValueOne:"+Math.abs(intValueOne));
-        System.out.println("Absolute value of integerValueTwo:"+Math.abs(intValueTwo));
-        System.out.println("Absolute value of doubleValueOne:"+Math.abs(doubleValueOne));
+//        System.out.println("Absolute value of integerValueThree:"+Math.abs(intValueThree));
+//        System.out.println("Absolute value of integerValueTwo:"+Math.abs(intValueTwo));
+        System.out.println("Absolute value of doubleValueThree:"+Math.abs(doubleValueOne));
         System.out.println("Absoulate value of doubleValueTwo:"+Math.abs(doubleValueTwo));
 
 
@@ -28,6 +30,7 @@ public class MathLearning {
 
 //        Example 4: Math.random example
         System.out.println(Math.random());
+
 
 
 
